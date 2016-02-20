b404df3ba4f4f5680d60291e166c069cac9d8a92
wenbiaoxing@yahoo.com <wenbiaoxing@yahoo.com>
2016-02-20 15:31:41 -0800

    add common module

---
+++ src/java/main/net/xingws/common/constants/Constants.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package net.xingws.common.constants;
+
+/**
+ * define the constants for all the module to use
+ * @author benxing
+ *
+ */
+public class Constants {
+		//constants for zookeeper master node
+		public static final String MASTER_NODE = "/master";
+		
+		//constants for zookeeper worker node
+		public static final String WORKERS_NODE = "/workers";
+		
+		//constants for zookeeper assignments node
+		public static final String ASSIGNMENTS_NODE = "/assignments";
+		
+		//constants for zookeeper tasks node
+		public static final String TASKS_NODE = "/tasks";
+		
+		//contants for zookeeper task status node
+		public static final String TASKS_STATUS_NODE = "/tasks_status";	
+}
